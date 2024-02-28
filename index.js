5f7d9aa5cd44f92e162d15bb63d5e07229cff013
Azfar Imam <azfarimam785@gmail.com>
2024-02-28 11:40:11 +0530

    first commit

--- index.js
+++ index.js
@@ -1,27 +1,16 @@
-// const http = require("http");
-// http
-//   .createServer((req, resp) => {
-//     resp.writeHead(200, { "Content-Type": "applicationjson" });
-//     resp.write(
-//       JSON.stringify({ Name: "azfar imam", email: "azfarimam785@gmail.com" })
-//     );
-//     resp.end;
-//   })
-//   .listen(9000);
-// console.log("hello");
+const express = require("express");
+const app = express();
 
-const http = require("http");
-const data = require("./data");
-http
-  .createServer((req, resp) => {
-    resp.writeHead(200, { "Content-Type": "application/json" });
-    resp.write(JSON.stringify(data));
-    resp.end(); // corrected to resp.end()
-  })
-  .listen(9000);
+app.get("", (req, resp) => {
+  resp.send("Hello, this is my home page");
+});
 
-console.log("Server running at http://localhost:9000/");
-const d = new Date();
-for (let index = 0; index < 10; index++) {
-  console.log(d.getSeconds());
-}
+app.get("/about", (req, resp) => {
+  resp.send("this is about page");
+});
+
+app.get("/help", (req, resp) => {
+  resp.send("This is a help desk");
+});
+
+app.listen(9999);
